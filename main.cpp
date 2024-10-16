0b5f811f9b0985bdacf70320f2c685f5b17a419d
root <root@DESKTOP-KOVAQT8.localdomain>
2024-10-16 13:08:01 +0300

    Create Function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 
+int ** CreateMatrix(size_t m, size_t n);
+
+void DeleteArray(int** t, size_t m, size_t n);
+
 int main()
 {
 	size_t M=0, N=0;
 	std::cin >>M>>N;
+	int **t = ct (M,N);
 	std::cout << M << " " << N << "/n";
 }
